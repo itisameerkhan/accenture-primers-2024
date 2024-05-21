0c5646ad621f10f4b57ce56b4e58331f1e14248a
itisameerkhan <itisameerkhan@gmail.com>
2024-05-21 11:45:24 +0530

    collectiosn

--- tekstac old modules/Java Programming/Collection, Generics and Stream API/ArrayList - Simple Handson1/UserInterface.java
+++ tekstac old modules/Java Programming/Collection, Generics and Stream API/ArrayList - Simple Handson1/UserInterface.java
@@ -1,5 +1,19 @@
 import java.util.*;
 
 public class UserInterface {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        List<String> list = new ArrayList<>();
+        System.out.print("Enter the Length: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+        for(int i=0;i<n;i++) {
+            String s = sc.nextLine();
+            list.add(s);
+        }
+        for(String s: list) {
+            System.out.println("String: " + s);
+        }
+        sc.close();
+    }
 }
