7d26caa1b8022a823d5c91f56984c7517262ec86
itisameerkhan <itisameerkhan@gmail.com>
2024-05-21 12:57:57 +0530

    RDBMS modules added

---
+++ tekstac old modules/RDBMS/Data Control Language/Create View_Mobile/model.sql
@@ -0,0 +1,8 @@
+
+CREATE VIEW customer_mobile_details AS 
+SELECT c.customer_id, c.customer_name, c.mobile, s.salesid, s.net_amount, m.model_name, m.manufacturer
+FROM customer_info AS c, sales_info AS s, mobile_master AS m
+WHERE c.customer_id = s.salesid 
+AND s.ime_no = m.ime_no
+ORDER BY c.customer_id, c.customer_name, s.salesid 
+ASC;
